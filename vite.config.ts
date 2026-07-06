793bdeafa99e06ccc195fb9d9e77ba4d42cd46cd
AnustupMaity <anustupmaity2004@gmail.com>
2026-07-06 21:29:35 +0530

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import electron from 'vite-plugin-electron-renderer';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    ...(process.env.ELECTRON ? [electron()] : []),
     react(),
     viteStaticCopy({
       targets: [
