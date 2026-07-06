f6e814dc48fc32c1e8cee80b29656d0ff8797684
AnustupMaity <anustupmaity2004@gmail.com>
2026-07-06 20:55:47 +0530

    Changes to be committed: 	deleted:    backend/regex.py
    Changes not staged for commit:
    	modified:   Dockerfile

--- backend/regex.py
+++
@@ -1,5 +0,0 @@
-"""
-Legacy wrapper for regex redaction functions.
-Redirects to pre-compiled ordered rules in app.services.regex_engine.
-"""
-from app.services.regex_engine import redact_all, ORDERED_PATTERNS
